6d85f5d0e5f0e79009fe96c613c9c1592d51f737
KM-github-source <72610085+KM-github-source@users.noreply.github.com>
2024-12-25 15:08:48 +0800

    Update run.sh

--- counselor/run.sh
+++ counselor/run.sh
@@ -1,2 +1,2 @@
 source /data/gaozhicheng/miniconda3/bin/activate llama3
-python run.py --file_dir "/nfs-data/spiderman/content/temp/" --save_dir "/nfs-data/spiderman/result/temp/" --CUDA_VISIBLE_DEVICES "3,4,5"
+python run.py --file_dir "/nfs-data/spiderman/content/2024-11-28/" --save_dir "/nfs-data/spiderman/result/2024-12-25/" --model_name "Qwen1.5-14B-Chat"  --model_dir "/nfs-data/zhengliwei/Projects/SHLP/LLMs/Qwen1.5-14B-Chat" --CUDA_VISIBLE_DEVICES "4,5,3"
