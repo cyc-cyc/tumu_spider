8ef3a099f985e975223e25ddfde45fbee1369560
KM-github-source <72610085+KM-github-source@users.noreply.github.com>
2024-12-25 15:08:20 +0800

    Update run.py

--- counselor/run.py
+++ counselor/run.py
@@ -7,6 +7,8 @@ def main():
     parser = argparse.ArgumentParser(description="Run information extraction on a specified directory.")
     parser.add_argument('--file_dir', type=str, required=True, help="The directory containing the files to process.")
     parser.add_argument('--save_dir', type=str, required=True, help="The directory to save the results.")
+    parser.add_argument('--model_name', type=str, required=True, help="The directory containing the files to process.")
+    parser.add_argument('--model_dir', type=str, required=True, help="The directory to save the results.")
     parser.add_argument('--CUDA_VISIBLE_DEVICES', type=str, required=True, help="CUDA_VISIBLE_DEVICES to set.")
 
     args = parser.parse_args()
@@ -16,6 +18,9 @@ def main():
         print(f"Error: The directory {args.file_dir} does not exist.")
         sys.exit(1)
 
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
     # 设置CUDA设备
     os.environ['CUDA_VISIBLE_DEVICES'] = args.CUDA_VISIBLE_DEVICES
 
@@ -23,7 +28,7 @@ def main():
     from extract_web_new import main as extract_main
 
     # 运行信息抽取
-    extract_main(file_dir=args.file_dir, save_dir=args.save_dir)
+    extract_main(file_dir=args.file_dir, save_dir=args.save_dir, model_name=args.model_name, model_dir=args.model_dir)
 
 if __name__ == "__main__":
     main()
